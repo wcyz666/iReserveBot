6128cb5991d58b1650162fc659e513037346a0f3
Wang Cheng <wcyz666@126.com>
2015-04-27 13:51:35 +0800

    Enhancement:
        1. Some bugs fixed and code refactored
        2. "Save and Fill" button can now work.

--- content.js
+++ content.js
@@ -1,22 +1,79 @@
 /**
  * Created by lenovo on 2015/4/6.
  */
-chrome.runtime.onMessage.addListener(function (message) {
-    console.log(message);
-});
-
-var OCRcode;
-
 (function init(){
-    window.onload = function () {
+
+    var MyUtils = (function (){
+
+        var ExtentionID = "gldpkdbfaapjnjlbajhhnjgpkamnllcn";
+
+        return {
+            ExtId : ExtentionID,
+            el : function(id, rg){
+                var range = rg || document;
+                return range.getElementById(id);
+            },
+            getMsg: function (what) {
+                return messages[what];
+            },
+            qs : function(selector, rg){
+                var range = rg || document;
+                return range.querySelector(selector);
+            },
+            qsa : function(selector, rg){
+                var range = rg || document;
+                return range.querySelectorAll(selector);
+            }
+        };
+    })();
+
+    var allowedType = ["email", "password", "text"];
+
+    chrome.runtime.onMessage.addListener(function (messages, sender, sendResponse) {
+        var key,
+            item,
+            message = {};
+        try{
+            message[messages.form_id] = "form_id";
+            message[messages.login_id] = messages["login_value"];
+            message[messages.password_id] = messages["password_value"];
+            message[messages.captcha_value] = getOCR(messages["captcha_pic_id"]);
+        }catch (ex){
+            sendResponse({error: "Invalid value."});
+            return true;
+        }
+
+        console.log(message);
+        for (key in message){
+            if (message[key].length < 1){
+                sendResponse({error: "Invalid value."});
+                return true;
+            }
+            item = MyUtils.el(key);
+            if (item && item.tagName.toLowerCase() == "form")
+                continue;
+            if (item && item.tagName.toLowerCase() == "input" && allowedType.indexOf(item.type.toLowerCase()) != -1){
+                item.value = message[key];
+            }
+            else{
+                sendResponse({error: "Can't find target DOM"});
+                return true;
+            }
+        }
+        sendResponse({status: "success"});
+    });
+
+
+    function getOCR(captcha_id) {
         var image = new Image();
-        image.src = document.getElementById("captcha").src;
+        image.src = document.getElementById(captcha_id).src;
         var canvas = document.createElement('canvas');
         canvas.height = image.height;
         canvas.width = image.width;
         var imgDraw = canvas.getContext('2d');
         imgDraw.drawImage(image,0,0);
-        OCRcode = OCRAD(imgDraw);
+        return OCRAD(imgDraw);
+
     }
 
 })();
--- popup.js
+++ popup.js
@@ -1,66 +1,64 @@
 /**
  * Created by lenovo on 2015/4/25.
  */
-var MyUtils = (function (){
-
-    var keys = ["form_id", "login_id", "login_value", "password_id", "password_value", "captcha_pic_id", "captcha_value"];
-    var messages = {
-        saveSuccessMsg : "<div class='alert alert-success alert-dismissible' role='alert'><strong>Well done!</strong> You have saved all input.</div>",
-        saveFillSuccessMsg : "<div class='alert alert-success alert-dismissible' role='alert'><strong>Well done!</strong> You have saved and filled all input.</div>",
-        warningMsg : "<div class='alert alert-warning alert-dismissible' role='alert'><strong>Warning!</strong> Better check yourself, you're not looking too good.</div>"
-    };
-    var ExtentionID = "gldpkdbfaapjnjlbajhhnjgpkamnllcn";
-    var saveItem = function(name, value){
-        localStorage.setItem(name, value);
-    };
-
-    var getItem = function(name){
-        return localStorage.getItem(name);
-    };
-
-    return {
-        ExtId : ExtentionID,
-        getKeys: function() {
-            return keys.slice(0);
-        },
-        getAll: function(){
-            var params = {},
-                item;
-
-            for (var i = 0; i < keys.length; i++){
-                item = getItem(keys[i]);
-                if (item)
-                    params[keys[i]] = item;
-            }
-
-            return params;
-        },
-        saveAll: function(params){
-            for (var key in params){
-                if (params.hasOwnProperty(key))
-                    saveItem(key, params[key]);
-            }
-        },
-        el : function(id, rg){
-            var range = rg || document;
-            return range.getElementById(id);
-        },
-        getMsg: function (what) {
-            return messages[what];
-        },
-        qs : function(selector, rg){
-            var range = rg || document;
-            return range.querySelector(selector);
-        },
-        qsa : function(selector, rg){
-            var range = rg || document;
-            return range.querySelectorAll(selector);
-        }
-    };
-})();
-
 (function init(){
+    var MyUtils = (function (){
 
+        var keys = ["form_id", "login_id", "login_value", "password_id", "password_value", "captcha_pic_id", "captcha_value"];
+        var messages = {
+            saveSuccessMsg : "<div class='alert alert-success alert-dismissible' role='alert'><strong>Well done!</strong> You have saved all input.</div>",
+            saveFillSuccessMsg : "<div class='alert alert-success alert-dismissible' role='alert'><strong>Well done!</strong> You have saved and filled all input.</div>",
+            warningMsg : "<div class='alert alert-warning alert-dismissible' role='alert'><strong>Warning!</strong> Better check yourself, you're not looking too good.</div>"
+        };
+        var ExtentionID = "gldpkdbfaapjnjlbajhhnjgpkamnllcn";
+        var saveItem = function(name, value){
+            localStorage.setItem(name, value);
+        };
+
+        var getItem = function(name){
+            return localStorage.getItem(name);
+        };
+
+        return {
+            ExtId : ExtentionID,
+            getKeys: function() {
+                return keys.slice(0);
+            },
+            getAll: function(){
+                var params = {},
+                    item;
+
+                for (var i = 0; i < keys.length; i++){
+                    item = getItem(keys[i]);
+                    if (item)
+                        params[keys[i]] = item;
+                }
+
+                return params;
+            },
+            saveAll: function(params){
+                for (var key in params){
+                    if (params.hasOwnProperty(key))
+                        saveItem(key, params[key]);
+                }
+            },
+            el : function(id, rg){
+                var range = rg || document;
+                return range.getElementById(id);
+            },
+            getMsg: function (what) {
+                return messages[what];
+            },
+            qs : function(selector, rg){
+                var range = rg || document;
+                return range.querySelector(selector);
+            },
+            qsa : function(selector, rg){
+                var range = rg || document;
+                return range.querySelectorAll(selector);
+            }
+        };
+    })();
     window.addEventListener("load", function(){
 
         var form = document.forms[0],
@@ -80,22 +78,23 @@ var MyUtils = (function (){
 
                 var keys = MyUtils.getKeys(),
                     i;
-                params = {}
+                params = {};
                 for (i = 0; i < keys.length; i++){
                     params[keys[i]] = form.elements[keys[i]].value;
                 }
 
                 MyUtils.saveAll(params);
-                MyUtils.el("messages").innerHTML = MyUtils.getMsg("saveSuccessMsg");
                 callback && callback.apply(null);
             }
         };
-        MyUtils.el("save").onclick = saveEvent();
+        MyUtils.el("save").onclick = saveEvent(function(){
+            MyUtils.el("messages").innerHTML = MyUtils.getMsg("saveSuccessMsg");
+        });
         MyUtils.el("fill-save").onclick = saveEvent(function(){
             chrome.tabs.query({active:	true, currentWindow: true},
                 function(tabs)	{
                     chrome.tabs.sendMessage(tabs[0].id, params, function(response){
-                        MyUtils.el("messages").innerHTML = response && response.error ? MyUtils.getMsg("warningMsg") : MyUtils.getMsg("saveFillSuccessMsg ");
+                        MyUtils.el("messages").innerHTML = ((response && response.error) ? MyUtils.getMsg("warningMsg") : MyUtils.getMsg("saveFillSuccessMsg"));
                     });
                 });
         });
